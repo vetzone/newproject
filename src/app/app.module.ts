2696c189184d5fff1f8dd03e572f2c984ce54a95
Adam <adamnotfound@gmail.com>
2019-10-29 20:15:52 +0200

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InfoComponent } from './core/info/info.component';
@@ -17,7 +18,16 @@ import { SignUpComponent } from './sign-up/SignUpComponent';
 import { FooterComponent } from './core/footer/footer.component';
 import { HeadComponent } from './core/head/head.component';
 import { HomeComponent } from './core/home/home.component';
+let config = new AuthServiceConfig([
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("547062139391402")
+  }
+]);
 
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -42,9 +52,15 @@ import { HomeComponent } from './core/home/home.component';
     RouterModule,
     FormsModule,
     ReactiveFormsModule,
+    SocialLoginModule
 
   ],
-  providers: [],
+    providers: [
+      {
+        provide: AuthServiceConfig,
+        useFactory: provideConfig
+      }
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/info/callus/callus.component.ts
+++ src/app/core/info/callus/callus.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService, SocialUser } from "angularx-social-login";
+import {FacebookLoginProvider, GoogleLoginProvider} from "angularx-social-login";
+
 
 @Component({
   selector: 'app-callus',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./callus.component.css']
 })
 export class CallusComponent implements OnInit {
-
-  constructor() { }
+   user: SocialUser;
+   loggedIn:boolean;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+
+    this.authService.authState.subscribe((user)=>{
+      this.user = user;
+      this.loggedIn = (user != null);
+    })
   }
 
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+  signOut(): void {
+    this.authService.signOut();
+    this.user = {};
+  }
 }
