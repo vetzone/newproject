5dd399647b5cf0779bb46416775143722f02661c
Adam <adamnotfound@gmail.com>
2019-10-29 20:43:23 +0200

    Updating

--- src/app/core/info/callus/callus.component.ts
+++ src/app/core/info/callus/callus.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService, SocialUser } from "angularx-social-login";
-import {FacebookLoginProvider, GoogleLoginProvider} from "angularx-social-login";
+
 
 
 @Component({
@@ -9,26 +8,12 @@ import {FacebookLoginProvider, GoogleLoginProvider} from "angularx-social-login"
   styleUrls: ['./callus.component.css']
 })
 export class CallusComponent implements OnInit {
-   user: SocialUser;
-   loggedIn:boolean;
-  constructor(private authService: AuthService) { }
+
+  constructor() { }
 
   ngOnInit() {
 
-    this.authService.authState.subscribe((user)=>{
-      this.user = user;
-      this.loggedIn = (user != null);
-    })
+
   }
 
-  signInWithFB(): void {
-    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
-  }
-  signInWithGoogle(): void {
-    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
-  }
-  signOut(): void {
-    this.authService.signOut();
-    this.user = {};
-  }
 }
