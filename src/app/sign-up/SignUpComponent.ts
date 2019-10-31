647b624a21d5ff530480717346dcdc737d900dbe
alyaamostafa-web <alyaamostafa@gmail.com>
2019-10-31 23:34:58 +0200

    change in sign up

--- src/app/sign-up/SignUpComponent.ts
+++ src/app/sign-up/SignUpComponent.ts
@@ -8,6 +8,9 @@ export class SignUpComponent implements OnInit {
  
   join() {
   }
+  onSubmit(){
+    
+  }
   emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   constructor() { }
   ngOnInit() {
